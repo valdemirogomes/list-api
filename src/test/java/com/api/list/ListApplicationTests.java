746bfdca2b4a2416224be295dc213cb72141d424
Valdemiro Gomes <valdemiro.gomes.rodrigues@gmail.com>
2024-11-12 19:17:45 -0300

    project creation

---
+++ src/test/java/com/api/list/ListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
